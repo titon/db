de37a2b874ec4229c3cf7c77538ab08f350d051d
Miles Johnson <mileswjohnson@gmail.com>
2013-07-14 13:36:57 -0700

    Added new exception

---
+++ src/Titon/Model/Exception/UnsupportedFeatureException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\Model\Exception;
+
+/**
+ * Exception thrown for features that aren't supported by a driver.
+ *
+ * @package Titon\Model\Exception
+ */
+class UnsupportedFeatureException extends \OutOfRangeException {
+
+}
