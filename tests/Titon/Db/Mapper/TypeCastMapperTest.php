0f9b1fb2253678214867cec159ac75d822062372
Miles Johnson <mileswjohnson@gmail.com>
2014-01-23 20:13:50 -0800

    Added TypeCast test

---
+++ tests/Titon/Db/Mapper/TypeCastMapperTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @copyright   2010-2013, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Db;
+
+use Titon\Db\Mapper\TypeCastMapper;
+use Titon\Test\Stub\Repository\Stat;
+use Titon\Test\TestCase;
+
+/**
+ * Test class for Titon\Db\Mapper\TypeCastMapper.
+ */
+class TypeCastMapperTest extends TestCase {
+
+    /**
+     * Test that fields are type cast.
+     */
+    public function testAfter() {
+        $mapper = new TypeCastMapper();
+        $mapper->setRepository(new Stat());
+
+        $data = [
+            'name' => 'foo',
+            'health' => '123',
+            'energy' => 456,
+            'damage' => '789.15',
+            'defense' => '12.45',
+            'range' => '22.25',
+            'isMelee' => '0',
+        ];
+
+        $this->assertSame([[
+            'name' => 'foo',
+            'health' => 123,
+            'energy' => 456,
+            'damage' => 789.15,
+            'defense' => 12.45,
+            'range' => 22.25,
+            'isMelee' => false
+        ]], $mapper->after([$data]));
+    }
+
+}
