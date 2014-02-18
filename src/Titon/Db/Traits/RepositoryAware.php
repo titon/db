78cfcbb2259feb1b9a769e6c73dd36eec892211f
Miles Johnson <mileswjohnson@gmail.com>
2014-02-17 16:43:54 -0800

    Updated copyright date

--- src/Titon/Db/Traits/RepositoryAware.php
+++ src/Titon/Db/Traits/RepositoryAware.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright   2010-2013, The Titon Project
+ * @copyright   2010-2014, The Titon Project
  * @license     http://opensource.org/licenses/bsd-license.php
  * @link        http://titon.io
  */
